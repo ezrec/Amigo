5d34925c4e3602b2817f152339834f15b301b677
Jason S. McMullan <jason.mcmullan@gmail.com>
2010-08-05 00:26:56 -0400

    [ucore.c] Extend the text program to attempt boot ROM reads
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>

--- ucore.c
+++ ucore.c
@@ -1,8 +1,13 @@
 
 void __start(void)
 {
-	volatile unsigned long * const stuff = (void *)0x00FC0000;
+	volatile unsigned long * const wcs = (void *)0x00FC0000;
+	volatile unsigned long * const rom = (void *)0x00F80000;
+	unsigned long tmp;
 
-	for (;;)
-		*stuff = 0xdeadcafe;
+	for (;;) {
+		*wcs = 0xcafebeef;
+		tmp = *wcs;
+		tmp = *rom;
+	}
 }
