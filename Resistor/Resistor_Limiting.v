3aa31f24b283846b4d7dd80e6ccc385b3590b777
Jason S. McMullan <jason.mcmullan@gmail.com>
2010-08-09 23:48:19 -0400

    [Resistor] Add some resistor models
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>

---
+++ Resistor/Resistor_Limiting.v
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2010, Jason S. McMullan. All rights reserved.
+ * Author: Jason S. McMullan <jason.mcmullan@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+`timescale 1ns/1ps
+
+module Resistor_Limiting #(
+	parameter ohms = 0,
+	parameter volts = 5.0
+) (
+	input 	I,
+	output	O
+);
+
+assign O = I;
+
+endmodule
